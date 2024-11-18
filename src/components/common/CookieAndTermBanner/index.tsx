097fb5dba1379f3358e129febe75a5dc2e6c69f7
ajimeno04 <jimenobeltran11@gmail.com>
2024-11-18 10:00:12 +0100

    fix: remove un used imports

--- src/components/common/CookieAndTermBanner/index.tsx
+++ src/components/common/CookieAndTermBanner/index.tsx
@@ -17,7 +17,6 @@ import { selectCookieBanner, openCookieBanner, closeCookieBanner } from '@/store
 
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
-import ExternalLink from '../ExternalLink'
 import Link from 'next/link'
 // import { AppRoutes } from '@/config/routes'
 // import ExternalLink from '../ExternalLink'
